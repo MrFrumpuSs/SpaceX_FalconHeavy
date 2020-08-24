3fbf60d4a1b309799c2cea3a39e904e4900b235c
Alexandr <MrFrumpuSsLive@gmail.com>
2020-08-24 22:00:58 +0300

    Yandex/Opera video fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var instance = new vidbg('.video', {
-    mp4: 'video/world.mp4', // URL or relative path to MP4 video
     webm: 'video/world.webm', // URL or relative path to webm video
+    mp4: 'video/world.mp4', // URL or relative path to MP4 video
     poster: 'img/worlderrorload.png', // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
   });
@@ -9,3 +9,6 @@ var rellax = new Rellax('.rellax');
 if (document.body.clientWidth < 767) {
   rellax.destroy();
 }
+
+var videos = document.querySelector('video');
+videos.setAttribute("pip", false)
