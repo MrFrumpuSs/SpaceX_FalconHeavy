4d8d1f6d204339fb94508b009b6807e53c8fa18c
Alexandr <MrFrumpuSsLive@gmail.com>
2020-08-26 00:32:05 +0300

    Favicon + Schema.org

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var instance = new vidbg('.video', {
     webm: 'video/world.webm', // URL or relative path to webm video
     mp4: 'video/world.mp4', // URL or relative path to MP4 video
-    poster: 'img/worlderrorload.png', // URL or relative path to fallback image
+    poster: 'img/worlderrorload.jpg', // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
   });
 
